bb382512d0e23a8dd570e310da75915adff845aa
Aman Jakkani <aj280598@gmail.com>
2020-03-23 15:44:45 -0500

    last updates

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # bed-recognition-nn
 Neural Network for recognizing the action of getting into/out of bed
 *with LFS capabilities added*
-https://youtu.be/ANem-8KwmDk
-
--- test.py
+++ test.py
@@ -7,11 +7,13 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from keras.utils import np_utils
 import numpy as np
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from skimage.transform import resize
+import json
 
 model = load_model('bedmodel.h5')
 #get test data 
@@ -36,4 +38,105 @@ print(x_test.shape, y_test.shape)
 
 results = model.evaluate(x_test, y_test)
 predictions = model.predict_classes(test_imgs)
-print(results, predictions)
+print("Testing Loss: ", results[0])
+print("Testing Accuracy: ", results[1])
+
+#you can edit the code below to include your test video's specific frame numbers, or simply test one video at a time
+
+#label prediction for video 1 (frames 0 - 20)
+print("Label predictions Test Video 1: ",predictions[0:21])
+length_vid1 = len(predictions[0:21])
+xtimes = np.arange(0,(length_vid1)/2, 0.5)
+ylabels = predictions[0:21]
+data = {}
+data["getting into/out of bed"] = []
+for i in range(len(xtimes)):
+    data["getting into/out of bed"].append([float(xtimes[i]), float(ylabels[i])])
+with open("timeLabel_1.json", 'w') as file:
+    json.dump(data, file)
+plt.xlabel("Time in Video (s)")
+plt.ylabel("Detection (0 - Not detecting, 1 - Detecting)")
+plt.title("Detection for Test Video 1")
+plt.plot(np.arange(0,(length_vid1)/2, 0.5), predictions[0:21])
+plt.savefig("test-vid-1.jpg")
+plt.clf()
+
+#label prediction for video 2 (frames 21 - 33)
+print("Label predictions Test Video 2: ",predictions[21:34])
+length_vid2 = len(predictions[21:34])
+xtimes = np.arange(0,(length_vid2)/2, 0.5)
+ylabels = predictions[21:34]
+data = {}
+data["getting into/out of bed"] = []
+for i in range(len(xtimes)):
+    data["getting into/out of bed"].append([float(xtimes[i]), float(ylabels[i])])
+with open("timeLabel_2.json", 'w') as file:
+    json.dump(data, file)
+plt.xlabel("Time in Video (s)")
+plt.ylabel("Detection (0 - Not detecting, 1 - Detecting)")
+plt.title("Detection for Test Video 2")
+plt.plot(np.arange(0,(length_vid2)/2, 0.5), predictions[21:34])
+x1,x2,y1,y2 = plt.axis()
+plt.savefig("test-vid-2.jpg")
+plt.clf()
+
+#label prediction for video 3 (frames 34 - 51)
+print("Label predictions Test Video 3: ",predictions[34:52])
+length_vid3 = len(predictions[34:52])
+xtimes = np.arange(0,(length_vid3)/2, 0.5)
+ylabels = predictions[34:52]
+data = {}
+data["getting into/out of bed"] = []
+for i in range(len(xtimes)):
+    data["getting into/out of bed"].append([float(xtimes[i]), float(ylabels[i])])
+with open("timeLabel_3.json", 'w') as file:
+    json.dump(data, file)
+plt.xlabel("Time in Video (s)")
+plt.ylabel("Detection (0 - Not detecting, 1 - Detecting)")
+plt.title("Detection for Test Video 3")
+plt.plot(np.arange(0,(length_vid3)/2, 0.5), predictions[34:52])
+x1,x2,y3,y4 = plt.axis()
+plt.axis((x1, x2, y1, y2))
+plt.savefig("test-vid-3.jpg")
+plt.clf()
+
+#label prediction for video 4 (frames 52 - 74)
+print("Label predictions Test Video 4: ",predictions[52:75])
+length_vid4 = len(predictions[52:75])
+plt.xlabel("Time in Video (s)")
+plt.ylabel("Detection (0 - Not detecting, 1 - Detecting)")
+plt.title("Detection for Test Video 4")
+xtimes = np.arange(0,(length_vid4)/2, 0.5)
+ylabels = predictions[52:75]
+data = {}
+data["getting into/out of bed"] = []
+for i in range(len(xtimes)):
+    data["getting into/out of bed"].append([float(xtimes[i]), float(ylabels[i])])
+with open("timeLabel_4.json", 'w') as file:
+    json.dump(data, file)
+plt.plot(np.arange(0,(length_vid4)/2, 0.5), predictions[52:75])
+x1,x2,y3,y4 = plt.axis()
+plt.axis((x1, x2, y1, y2))
+plt.savefig("test-vid-4.jpg")
+plt.clf()
+
+#label prediction for video 5 (frames 75 - 103)
+print("Label predictions Test Video 5: ",predictions[75:103])
+length_vid5 = len(predictions[75:103])
+xtimes = np.arange(0,(length_vid5)/2, 0.5)
+ylabels = predictions[75:103]
+data = {}
+data["getting into/out of bed"] = []
+for i in range(len(xtimes)):
+    data["getting into/out of bed"].append([float(xtimes[i]), float(ylabels[i])])
+with open("timeLabel_5.json", 'w') as file:
+    json.dump(data, file)
+plt.xlabel("Time in Video (s)")
+plt.ylabel("Detection (0 - Not detecting, 1 - Detecting)")
+plt.title("Detection for Test Video 5")
+plt.plot(np.arange(0,(length_vid5)/2, 0.5), predictions[75:103])
+x1,x2,y3,y4 = plt.axis()
+plt.axis((x1, x2, y1, y2))
+plt.savefig("test-vid-5.jpg")
+plt.close()
+
